d5c23006500cf623dd86945c0eb7fd18a87d9669
Bruno Pereira <bapereir@pmintl.net>
2021-10-12 01:00:50 +0100

    build: created sidebar component with the genre menu logic.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+import { Button } from "../components/Button";
+import { api } from "../services/api";
+
+import "../styles/sidebar.scss";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+interface Props {
+  handleClick: (id: number) => void;
+  selectedGenreId: number;
+}
+export function SideBar({ handleClick, selectedGenreId }: Props) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
